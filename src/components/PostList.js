780d8cf6dcbbb3188ca70c1aa8297bab5945a3f0
David Wachlin <davidwachlin@gmail.com>
2019-12-30 21:19:07 -0500

    adds map state to props to post list componenent

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,9 +7,15 @@ class PostList extends Component {
 		this.props.fetchPosts();
 	}
 	render() {
+		console.log(this.props)
+		console.log(this.props.posts)
 		return <div>Post List</div>;
 	}
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+	return { posts: state.posts }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
 
