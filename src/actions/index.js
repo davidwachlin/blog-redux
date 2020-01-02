5cd3e2adbf8988dd28996a716773e48bdfa3a15e
David Wachlin <davidwachlin@gmail.com>
2020-01-02 17:50:38 -0500

    refactors fetchPostsAndUsers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+	// eslint-disable-next-line no-undef
+	const userIds = _.uniq(_.map(getState().posts, 'userId'));
+	userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
 	const response = await jsonPlaceholder.get('/posts');
 
@@ -11,7 +18,7 @@ export const fetchUser = id => async dispatch => {
 	const response = await jsonPlaceholder.get(`/users/${id}`);
 
 	dispatch({ type: 'FETCH_USER', payload: response.data });
-}
+};
 
 // Memoized Version
 // export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
